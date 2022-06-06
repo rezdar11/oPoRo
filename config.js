4166c7227421fef91d439c07f1d8c39bda602c24
rezdar11 <107009582+rezdar11@users.noreply.github.com>
2022-06-07 02:49:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `HaaaMa / Prefix`;
-exports.Token = `HaaaMa / Token`;
+exports.Prefix = `1`;
+exports.Token = `OTcxMTg0MTUyMTAxNjY2ODI3.Gaj1w4.NJ6k9KRz-tqC-DGe5sMRGZJny7GFXIsHkt_7H8`;
 //// token dabe Bashi haaama.js dabne
